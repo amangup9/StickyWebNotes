6eaa9dd4b0f68612a07d6a76ff2fc3779fbf07a7
Kamu Malinen <dev@kamalin.fi>
2026-01-31 13:12:38 +0200

    Implement contextMenus usage.

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,15 @@
 chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+      id: "addNote",
+      title: "Add note",
+      contexts: ["all"]
+    });
+
+    chrome.contextMenus.onClicked.addListener((info, tab) => {
+      chrome.tabs.sendMessage(tab.id, {
+        action: "ADD_NOTE_CLICKED", // Defensive programming
+      });
+    });
+
     console.log('Sticky Notes Extension Installed');
   });
--- scripts/content.js
+++ scripts/content.js
@@ -29,11 +29,25 @@ var myNotesArray = new Array();
 
 
   // Create a new note
+/*
   document.addEventListener('dblclick', (e) => {
     const id = 'note-' + new Date().getTime();
     const position = { x: e.pageX, y: e.pageY };
     createNoteElement(id, '', position);
   });
+*/
+  let clickPos = { x: 0, y: 0 };
+  document.addEventListener("contextmenu", (e) => {
+    clickPos = { x: e.clientX, y: e.clientY };
+  });
+
+  chrome.runtime.onMessage.addListener((req, sender, res) => {
+    if (req.action == "ADD_NOTE_CLICKED") { // Defensive programming
+      console.log("Adding note through context menu.");
+      const id = 'note-' + new Date().getTime();
+      createNoteElement(id, '', clickPos);
+    }
+  })
 
   function createNoteElement(id, text, position) {
 
